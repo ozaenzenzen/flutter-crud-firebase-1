234f0445ea3d6e22cda4744f109917ffce8c503b
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-24 09:53:02 +0700

    add rate calculation model

---
+++ lib/model/ratecalulation_model.dart
@@ -0,0 +1,13 @@
+class RateCalculationModel {
+  int? id;
+  String? name;
+  String? desc;
+  int? stars;
+
+  RateCalculationModel({
+    this.id,
+    this.name,
+    this.desc,
+    this.stars,
+  });
+}
