61375da120c5e6bd42ff70cdd3e277484d59db3a
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-24 09:53:07 +0700

    add rate calculation data

---
+++ lib/data/ratecalculation_data.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_crud_firebase_1/model/ratecalulation_model.dart';
+
+class RateCalculationData {
+  List _data = [
+    RateCalculationModel(
+      id: 0,
+      name: "Budi",
+      desc: "Good Job..",
+      stars: 5,
+    ),
+    RateCalculationModel(
+      id: 1,
+      name: "Alex",
+      desc: "Good Job..",
+      stars: 5,
+    ),
+    RateCalculationModel(
+      id: 2,
+      name: "Jones",
+      desc: "Good Job..",
+      stars: 5,
+    ),
+    RateCalculationModel(
+      id: 3,
+      name: "George",
+      desc: "Good Job..",
+      stars: 4,
+    ),
+    RateCalculationModel(
+      id: 4,
+      name: "Francis",
+      desc: "Good Power..",
+      stars: 5,
+    ),
+  ];
+
+  List get data {
+    return [..._data];
+  }
+}
