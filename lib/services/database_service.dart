c5cc7478969e8f7f3ad32294b1cb26a839c36c56
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-27 22:44:29 +0700

    update database service

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,15 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_crud_firebase_1/model/account_model.dart';
 
 class DatabaseService {
   static CollectionReference collectionReference =
       FirebaseFirestore.instance.collection('account');
 
-  Future createObjectAccount(id, {name, desc, stars}) async {
-    await collectionReference.doc(id).set({
-      'id': id,
-      'name': name,
-      'desc': desc,
-      'stars': stars,
+  Future createObjectAccount(AccountModel accountModel) async {
+    // Future createObjectAccount(id, {name, title, email, citizen, aboutMe}) async {
+    await collectionReference.doc(FieldValue.increment(1).toString()).set({
+      'id': FieldValue.increment(1),
+      'name': accountModel.name,
+      'title': accountModel.title,
+      'email': accountModel.email,
+      'citizen': accountModel.citizen,
+      'aboutMe': accountModel.aboutMe,
     });
+  }
+
+  Future<DocumentSnapshot> getDataAccount(id) async {
+    return await collectionReference.doc(id).get();
   }
 }
