98e0437e206502dc6e13c23bd06fe553e6d12298
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-27 22:44:12 +0700

    add model

---
+++ lib/model/account_model.dart
@@ -0,0 +1,17 @@
+class AccountModel {
+  int? id;
+  String? name;
+  String? title;
+  String? email;
+  String? citizen;
+  String? aboutMe;
+
+  AccountModel({
+    this.id,
+    this.name,
+    this.title,
+    this.email,
+    this.citizen,
+    this.aboutMe,
+  });
+}
