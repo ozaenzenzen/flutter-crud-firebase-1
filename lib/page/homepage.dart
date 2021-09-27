5628880133460ec329455c4c125dccc89a9620b8
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-27 21:50:40 +0700

    update homepage dart

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud_firebase_1/page/adddata_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,19 +11,55 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   ScreenUtil screenUtil = ScreenUtil();
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          "Home Page",
-          style: GoogleFonts.raleway(
-            fontSize: screenUtil.setSp(25),
-            fontWeight: FontWeight.w800,
+    return Stack(
+      children: [
+        Container(
+          child: Center(
+            child: Text(
+              "Home Page",
+              style: GoogleFonts.raleway(
+                fontSize: screenUtil.setSp(25),
+                fontWeight: FontWeight.w800,
+              ),
+            ),
           ),
         ),
-      ),
+        Positioned(
+          bottom: screenUtil.setHeight(10),
+          right: screenUtil.setHeight(10),
+          // alignment: Alignment.bottomRight,
+          child: ElevatedButton(
+            onPressed: () {
+              Get.to(() => AddDataPage());
+            },
+            child: Text("Add"),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40),
+              ),
+              minimumSize: Size(
+                screenUtil.setWidth(100),
+                screenUtil.setHeight(40),
+              ),
+              primary: Colors.pink.shade800,
+            ),
+          ),
+        ),
+      ],
     );
+    // return Container(
+    //   child: Center(
+    //     child: Text(
+    //       "Home Page",
+    //       style: GoogleFonts.raleway(
+    //         fontSize: screenUtil.setSp(25),
+    //         fontWeight: FontWeight.w800,
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
