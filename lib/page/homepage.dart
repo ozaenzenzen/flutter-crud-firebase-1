150f2fd0825a338ae543115c94a9d1fed4054792
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-29 06:43:48 +0700

    update homepage dart

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_crud_firebase_1/model/account_model.dart';
 import 'package:flutter_crud_firebase_1/page/adddata_page.dart';
 import 'package:flutter_crud_firebase_1/services/database_service.dart';
-import 'package:flutter_crud_firebase_1/utils/fam_images.dart';
+import 'package:flutter_crud_firebase_1/widget/homedata_item.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -34,51 +34,12 @@ class _HomePageState extends State<HomePage> {
                   itemBuilder: (context, index) {
                     AccountModel accountModelData = AccountModel.fromJson(
                         snapshot.data!.docs[index].data() as Map);
-                    return Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: screenUtil.setHeight(15),
-                        horizontal: screenUtil.setWidth(15),
-                      ),
-                      width: screenUtil.screenWidth,
-                      // height: screenUtil.setHeight(90),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(18),
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 2),
-                            color: Colors.pink.shade100,
-                            spreadRadius: 1,
-                            blurRadius: 6,
-                          ),
-                        ],
-                        color: Colors.white,
-                      ),
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Image.network(
-                              FAMImages.flutter_image,
-                              width: screenUtil.setWidth(60),
-                              height: screenUtil.setHeight(60),
-                            ),
-                          ),
-                          SizedBox(
-                            width: screenUtil.setWidth(5),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // Text("${snapshot.data!.docs[index].data()}"),
-                              Text("${accountModelData.name}"),
-                              Text("${accountModelData.title}"),
-                              Text("${accountModelData.email}"),
-                              Text("${accountModelData.citizen}"),
-                              Text("${accountModelData.aboutMe}"),
-                            ],
-                          ),
-                        ],
-                      ),
+                    return HomeDataItem(
+                      name: accountModelData.name,
+                      title: accountModelData.title,
+                      email: accountModelData.email,
+                      citizen: accountModelData.citizen,
+                      aboutMe: accountModelData.aboutMe,
                     );
                   },
                 );
@@ -100,7 +61,12 @@ class _HomePageState extends State<HomePage> {
           // alignment: Alignment.bottomRight,
           child: ElevatedButton(
             onPressed: () {
-              Get.to(() => AddDataPage());
+              Get.to(
+                () => AddDataPage(),
+                arguments: {
+                  'type': 'add',
+                },
+              );
             },
             child: Row(
               children: [
@@ -133,16 +99,5 @@ class _HomePageState extends State<HomePage> {
         ),
       ],
     );
-    // return Container(
-    //   child: Center(
-    //     child: Text(
-    //       "Home Page",
-    //       style: GoogleFonts.raleway(
-    //         fontSize: screenUtil.setSp(25),
-    //         fontWeight: FontWeight.w800,
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
