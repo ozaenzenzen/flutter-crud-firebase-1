733b20201d0568b9d042c4c666d81f75184fdfa6
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-28 08:33:12 +0700

    update database service

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -7,8 +7,7 @@ class DatabaseService {
 
   Future createObjectAccount(AccountModel accountModel) async {
     // Future createObjectAccount(id, {name, title, email, citizen, aboutMe}) async {
-    await collectionReference.doc(FieldValue.increment(1).toString()).set({
-      'id': FieldValue.increment(1),
+    await collectionReference.doc(accountModel.name).set({
       'name': accountModel.name,
       'title': accountModel.title,
       'email': accountModel.email,
@@ -19,5 +18,28 @@ class DatabaseService {
 
   Future<DocumentSnapshot> getDataAccount(id) async {
     return await collectionReference.doc(id).get();
+  }
+
+  Future<QuerySnapshot> getAllDataAccount() async {
+    // QuerySnapshot querySnapshot = await collectionReference.get();
+
+    // final allData = querySnapshot.docs.map((doc) => doc.data()!);
+    // // return allData;
+    return await collectionReference.get();
+  }
+
+  // Stream<DocumentSnapshot> getAllDataAccount2() {
+  Stream<QuerySnapshot> getAllDataAccount2() {
+    return collectionReference.snapshots();
+  }
+
+  Future updateDataAccount(AccountModel accountModel) async {
+    await collectionReference.doc(accountModel.name).update({
+      'name': accountModel.name,
+      'title': accountModel.title,
+      'email': accountModel.email,
+      'citizen': accountModel.citizen,
+      'aboutMe': accountModel.aboutMe,
+    });
   }
 }
