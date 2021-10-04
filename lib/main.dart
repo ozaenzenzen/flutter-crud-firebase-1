b57fe7cc2a7f71a5e056cdb8a67daad12a2d4502
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-04 21:44:12 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_firebase_1/mainpage.dart';
