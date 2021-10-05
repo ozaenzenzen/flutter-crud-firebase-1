c92fdbe441b2c622fa1fe634a370ce5ca0ce1696
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-06 05:40:21 +0700

    update dataitem services

--- lib/services/dataitem_service.dart
+++ lib/services/dataitem_service.dart
@@ -7,10 +7,10 @@ class DatabaseService {
       FirebaseFirestore.instance.collection('account');
   DocumentReference? docref;
 
-  Future createObjectAccount(AccountModel accountModel, String id) async {
+  Future createObjectAccount(AccountModel? accountModel, String id) async {
     // Future createObjectAccount(id, {name, title, email, citizen, aboutMe}) async {
     await collectionReference.doc(id).set({
-      'name': accountModel.name,
+      'name': accountModel!.name,
       'title': accountModel.title,
       'email': accountModel.email,
       'citizen': accountModel.citizen,
