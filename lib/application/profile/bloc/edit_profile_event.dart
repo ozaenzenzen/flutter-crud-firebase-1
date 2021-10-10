11592537db647f9ec6191b61f5b68fde9d8ce113
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-10 14:26:00 +0700

    update edit profile event

--- lib/application/profile/bloc/edit_profile_event.dart
+++ lib/application/profile/bloc/edit_profile_event.dart
@@ -3,4 +3,12 @@ part of 'edit_profile_bloc.dart';
 @immutable
 abstract class EditProfileEvent {}
 
-class EditProfileEventCreate extends EditProfileEvent {}
+class EditProfileEventUpdate extends EditProfileEvent {
+  final ProfileModel? profileModel;
+  final String? id;
+
+  EditProfileEventUpdate({
+    this.profileModel,
+    this.id,
+  });
+}
