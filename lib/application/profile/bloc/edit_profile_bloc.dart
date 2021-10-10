ec792ea0a4b6f365f64b921ed3c90f563ed17d6f
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-10 14:26:05 +0700

    update edit profile bloc

--- lib/application/profile/bloc/edit_profile_bloc.dart
+++ lib/application/profile/bloc/edit_profile_bloc.dart
@@ -1,13 +1,29 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_crud_firebase_1/model/profiledata_model.dart';
+import 'package:flutter_crud_firebase_1/services/dataitem_service.dart';
+import 'package:flutter_crud_firebase_1/services/editprofile_service.dart';
 import 'package:meta/meta.dart';
 
 part 'edit_profile_event.dart';
 part 'edit_profile_state.dart';
 
 class EditProfileBloc extends Bloc<EditProfileEvent, EditProfileState> {
-  EditProfileBloc() : super(EditProfileInitial()) {
-    on<EditProfileEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  EditProfileBloc() : super(EditProfileInitial());
+
+  Stream<EditProfileState> mapEventToState(EditProfileEvent event) async* {
+    if (event is EditProfileEventUpdate) {
+      yield EditProfileLoading();
+      try {
+        await EditProfileService()
+            .updateProfileData(event.profileModel!, event.id.toString())
+            .onError(
+              (error, stackTrace) => EditProfileError(
+                error.toString(),
+              ),
+            );
+      } catch (e) {
+        EditProfileError(e.toString());
+      }
+    }
   }
 }
