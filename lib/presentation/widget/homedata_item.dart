70f71654dc785291abd134612047e18a5cf60680
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-10-21 12:21:38 +0700

    update homedata item: add text style

--- lib/presentation/widget/homedata_item.dart
+++ lib/presentation/widget/homedata_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:flutter_crud_firebase_1/utils/fam_images.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeDataItem extends StatefulWidget {
   final String? id;
@@ -63,33 +64,66 @@ class _HomeDataItemState extends State<HomeDataItem> {
           ],
           color: Colors.white,
         ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Image.network(
-                FAMImages.flutter_image,
-                width: screenUtil.setWidth(60),
-                height: screenUtil.setHeight(60),
+        child: Container(
+          margin: EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.network(
+                  FAMImages.flutter_image,
+                  width: screenUtil.setWidth(60),
+                  height: screenUtil.setHeight(60),
+                ),
               ),
-            ),
-            SizedBox(
-              width: screenUtil.setWidth(5),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Text("${snapshot.data!.docs[index].data()}"),
-                  Text("${widget.name}"),
-                  Text("${widget.title}"),
-                  Text("${widget.email}"),
-                  Text("${widget.citizen}"),
-                  Text("${widget.aboutMe}"),
-                ],
+              SizedBox(
+                width: screenUtil.setWidth(5),
               ),
-            ),
-          ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Text("${snapshot.data!.docs[index].data()}"),
+                    Text(
+                      "${widget.name}",
+                      style: GoogleFonts.poppins(
+                        fontSize: screenUtil.setSp(20),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      "${widget.title}",
+                      style: GoogleFonts.poppins(
+                        fontSize: screenUtil.setSp(14),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenUtil.setHeight(10),
+                    ),
+                    Text(
+                      "${widget.email}",
+                      style: GoogleFonts.poppins(
+                        fontSize: screenUtil.setSp(11),
+                      ),
+                    ),
+                    Text(
+                      "${widget.citizen}",
+                      style: GoogleFonts.poppins(
+                        fontSize: screenUtil.setSp(11),
+                      ),
+                    ),
+                    Text(
+                      "${widget.aboutMe}",
+                      style: GoogleFonts.poppins(
+                        fontSize: screenUtil.setSp(11),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
